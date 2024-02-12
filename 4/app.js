710b9db63bc0b3ade59f2ae1ec289e3f0e9f28e0
Kefirchik99 <jermakent@gmail.com>
2024-02-12 19:33:50 +0200

    V 1.0

---
+++ 4/app.js
@@ -0,0 +1,21 @@
+let calculateArray = [];
+
+function populateArray(calculateArray) {
+  for (let i = 1; i <= 10; i++) {
+    calculateArray.push(i);
+  }
+}
+function additionFunction(total, value) {
+  return total + value;
+}
+
+populateArray(calculateArray);
+
+let sum = calculateArray.reduce(additionFunction, 0);
+
+console.log(sum);
+
+function displayResult(sum) {   
+    document.getElementById('sum-from-array').innerHTML = sum;
+}
+displayResult(sum);
