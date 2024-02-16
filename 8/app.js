82a6c1de6c2dc804994961d8e7d1e93167dbe364
Kefirchik99 <jermakent@gmail.com>
2024-02-16 21:49:35 +0200

    V 1.0

--- 8/app.js
+++ 8/app.js
@@ -1,25 +1,25 @@
 let userNumber = document.getElementById('initial_number').value
 
-function checkNumber (userNumber) {
- if (userNumber % 10 === 0) {
-     return true
- } else {
-     return false
- }
+function checkNumber(userNumber) {
+    if (userNumber % 10 === 0) {
+        return true
+    } else {
+        return false
+    }
 }
 
-document.getElementById('submit').addEventListener('click', function(){
+document.getElementById('submit').addEventListener('click', function () {
     var userNumber = document.getElementById('initial_number').value
-    if (userNumber!== '' && !isNaN(userNumber) && userNumber < 100) {
-    var isDivisibleBy10 = checkNumber(userNumber)
-    document.getElementById('t_or_f').innerHTML = isDivisibleBy10
+    if (userNumber !== '' && !isNaN(userNumber) && userNumber < 100) {
+        var isDivisibleBy10 = checkNumber(userNumber)
+        document.getElementById('t_or_f').innerHTML = isDivisibleBy10
     } else {
         document.getElementById('t_or_f').innerHTML = 'Please only enter a number not larger than 100'
     }
-    
+
 })
 
-function reset () {
+function reset() {
     document.getElementById('reset-1').onclick = function () {
         document.getElementById('t_or_f').innerHTML = ''
         document.getElementById('initial_number').value = ''
