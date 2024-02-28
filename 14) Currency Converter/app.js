1fb5fd2649b698864e71e46999576a772972004a
Kefirchik99 <jermakent@gmail.com>
2024-02-28 21:51:13 +0200

    V 0.2

--- 14) Currency Converter/app.js
+++ 14) Currency Converter/app.js
@@ -1,1 +1,6 @@
-p
+function calculatePrice() {
+    var price = document.getElementById("price").value;
+    var exchangeRate = document.getElementById("exchange-rate").value;
+    var result = price - (price * exchangeRate / 100);
+    document.getElementById("result-container").innerHTML = result;
+}
