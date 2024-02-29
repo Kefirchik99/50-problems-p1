171586dbf018f743688f288e35d312d274d0fd3a
Kefirchik99 <jermakent@gmail.com>
2024-02-29 19:15:59 +0200

    V 0.9

--- 14) Currency Converter/app.js
+++ 14) Currency Converter/app.js
@@ -1,6 +1,21 @@
+var exchangeRate = 80000;
+
 function calculatePrice() {
-    var price = document.getElementById("price").value;
-    var exchangeRate = document.getElementById("exchange-rate").value;
-    var result = price - (price * exchangeRate / 100);
-    document.getElementById("result-container").innerHTML = result;
+    var price = parseFloat(document.getElementById("input_amount").value);
+    if (isNaN(price)) {
+        document.getElementById("result-container").innerHTML = "";
+    } else {
+        var result = price * exchangeRate;
+        document.getElementById("result-container").innerHTML = "The amount BTC in EUR is:" + result.toLocaleString('en-US', { style: 'currency', currency: 'EUR' });
+    }
 }
+
+var inputField = document.createElement("input");
+inputField.id = "input_amount";
+inputField.type = "number";
+inputField.placeholder = "Enter BTC amount";
+document.getElementById("input-container").appendChild(inputField);
+
+
+
+document.getElementById("calculate_button").addEventListener("click", calculatePrice);
